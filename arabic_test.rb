f5a0d591564cd8b9dbc1ae7286ecb8f37abd3779
Jbrow95 <browningjustin95@gmail.com>
2018-10-15 16:27:04 -0400

    tested if IV equals 4

--- arabic_test.rb
+++ arabic_test.rb
@@ -7,4 +7,8 @@ class TestUntitled < Test::Unit::TestCase
     def test_if_I_equals_1
         assert_equal 1, "I".to_arabic
     end
+
+    def test_if_IV_equals_4
+        assert_equal 4, "IV".to_arabic
+    end
 end
