ea15c5c40344114ce3532cb573cd5920dd13c1cc
Jbrow95 <browningjustin95@gmail.com>
2018-10-15 14:55:32 -0400

    wrote a do loop to go through my hash and convert 1 to x

--- roman.rb
+++ roman.rb
@@ -1,7 +1,15 @@
 class Integer
    
     def to_roman
-        {}
+       data = {1 => 'I'}
+
+       num = self
+       roman = ""
+        data.each do |v, ltr|
+            roman << ltr * (num / v)
+            num = num % v
+        end
+        return roman
     end
 end
 
