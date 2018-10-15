d269e123f1e7c0a690b32e086e8c6a282687c3c7
Jbrow95 <browningjustin95@gmail.com>
2018-10-15 14:47:06 -0400

    test if x equals 1 failed

--- test.rb
+++ test.rb
@@ -6,4 +6,8 @@ class TestUntitled < Test::Unit::TestCase
     def test_if_data_equals_hash
         assert_equal 0.to_roman().class, Hash
     end
+
+    def test_if_1_equals_x
+        assert_equal 1.to_roman, 'X'
+    end
 end
