2caf86ebf1e9c4598de86ff76aa61c06dac9957b
Jbrow95 <browningjustin95@gmail.com>
2018-10-15 15:10:59 -0400

    added valie of 400 to hassh

--- roman.rb
+++ roman.rb
@@ -1,7 +1,7 @@
 class Integer
    
     def to_roman
-       data = {100 => "C", 90 => "XC", 50 => "L",  40 => "XL",  10 => "X",  9 => "IX", 5 => "V",  4 => "IV", 1 => 'I'}
+       data = {400 => "CD", 100 => "C", 90 => "XC", 50 => "L",  40 => "XL",  10 => "X",  9 => "IX", 5 => "V",  4 => "IV", 1 => 'I'}
 
        num = self
        roman = ""
